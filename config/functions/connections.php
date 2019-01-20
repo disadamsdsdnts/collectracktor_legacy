f91ef35cc8ca63a77c98a48ac4322878c25e2e56
Adan Martin <disadamsdsdnts@gmail.com>
2019-01-20 21:18:47 +0100

    Función para hacer un open a la base de datos

--- config/functions/connections.php
+++ config/functions/connections.php
@@ -1,13 +1,6 @@
 <?php
-    /* FUNCIONES GENERALES DE CONEXIÓN */
-    /* Abrir conexión a la base de datos */
 	function openConnection(){
-		include_once(DOCUMENT_ROOT . 'config/config.php');
-	}
+		global $databaseHostConnection, $databaseUserConnection, $databasePasswordConnection, $databaseNameConnection;
 
-    /* Cerrar la conexión a la base de datos */
-	function closeConnection(){
-        include_once(DOCUMENT_ROOT . 'config/config.php');
-		mysqli_close($databaseConnection);
-		unset($databaseConnection);
+		return mysqli_connect($databaseHostConnection, $databaseUserConnection, $databasePasswordConnection, $databaseNameConnection);
 	}
